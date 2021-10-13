4208b598bb8edb1c74c91760c88ab86f823aa3a6
Devonne Walker <devonnewalker@gmail.com>
2021-10-13 16:54:16 -0400

    Was able to get the conditional render to work

--- client/components/Lobby.js
+++ client/components/Lobby.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Game from "./Game";
 
 const Lobby = () => {
   return (
     <div>
       <h3>Welcome to the lobby!</h3>
+      <Game />
     </div>
   );
-}
+};
 
-export default Lobby
+export default Lobby;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,11 @@
-import Phaser from "phaser";
 import React from "react";
-import ReactDOM from 'react-dom';
-import App from '/client/App';
-import { BrowserRouter } from 'react-router-dom'
+import ReactDOM from "react-dom";
+import Routes from "../Routes";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-    document.getElementById('app')
-)
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>,
+  document.getElementById("app")
+);
