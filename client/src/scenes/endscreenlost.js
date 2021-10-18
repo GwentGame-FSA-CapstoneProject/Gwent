f8a2f78b123b3e3b7ceb222defeaac92d32a8c10
Samer Saleh <samersaleh@Samers-MacBook-Pro.local>
2021-10-18 11:28:49 -0400

    added endscreens

--- client/src/scenes/endscreenlost.js
+++ client/src/scenes/endscreenlost.js
@@ -6,11 +6,24 @@ export default class GameLost extends Phaser.Scene
   constructor(){
     super('GameLost')
   }
+
+  preload()
+  {
+    this.load.image('olgierd','/assets/olgierd.jpg')
+  }
   create()
   {
-   this.add.text(640,600,'You Lost!',{
+    this.add.image(640,1000,'olgierd')
+   this.add.text(640,200,'You Lost!',{
      fontSize: 90,
      color: 'DarkRed'
    }).setOrigin(0.5)
+  //  this.add.text(1100,200,"Press space to play again!",{
+  //    fontSize: 25
+  //  }).setOrigin(0.5)
+  //  this.input.keyboard.on('keydown-SPACE',()=>{
+  //    this.scene.start('Game')
+  //  })
+ }
   }
-}
+
