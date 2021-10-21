8f944830590f10a9ae069a6df397ee95cfc34e05
Isaac Scheff <isaac@Isaacs-MacBook-Pro.local>
2021-10-21 15:28:39 -0400

    Phaser sceen added for opponent disconnect.

--- client/src/scenes/opponentDisconnect.js
+++ client/src/scenes/opponentDisconnect.js
@@ -1,21 +1,20 @@
 import Phaser from "phaser";
 
-export default class GameWon extends Phaser.Scene
-{
+export default class GameWon extends Phaser.Scene{
   constructor(){
     super('GameWon')
   }
-  preload()
-  {
+
+  preload(){
     this.load.image('queen','/assets/queen.jpg')
   }
-  create()
-  {
+
+  create(){
     this.add.image(640,1000,'queen')
    this.add.text(640,200,'You Won!',{
      fontSize: 90,
      color: 'DarkGoldenRod'
    }).setOrigin(0.5)
+   this.add.text(200, 400,"Opponent Disconnected!").setFontSize(42);
   }
 }
-
